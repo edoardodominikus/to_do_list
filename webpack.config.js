0f49e76e4bea28bc0c90b8d9505cd43a4e0a7e78
edoardodominikus <s108006222@m108.nthu.edu.tw>
2021-07-02 11:21:02 +0800

    fixed the web.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,19 @@
-const path = require('path');
-
 module.exports = {
-    entry: './src/index.js',
     mode: 'development',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname,'dist'),
-    },
     module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use:['style-loader','css-loader'],
-            },
-        ],
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ],
     },
-
-
-
-
-};
+  };
